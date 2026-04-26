21952ef59595240ebce0f7bc751f7e467112630e
Edwin Bautista <bautistaedwin433@gmail.com>
2026-04-26 01:03:21 -0400

    Added splitting feature, renamed some of the labels, added a second row display label

--- stopwatchproject.asm
+++ stopwatchproject.asm
@@ -13,8 +13,6 @@
 .equ BTN_MODE = PORTD
 .equ BTN_START = PD2                    ; Port D Pin 2, INT0?
 .equ BTN_LAP = PD3                      ; Port D Pin 3, INT1?
-.equ BTN_RESET = PB3                    ; Port B Pin 11, does not use the two dedicated external interrupts
-                                        ; TODO: Configure reset button definitions + gpio init
                                         
 ; Flags defined, if flag is set to 1(set in the ISRs), then that interrupt happened
 .def tickFlag = r21                     ; every centisecond .01
@@ -30,6 +28,10 @@
 .equ centiseconds = 0x0100              ; (0-99)
 .equ seconds = 0x0101                   ; (0-59)
 .equ minutes = 0x0102                   ; (0-59)
+
+.equ lap_centiseconds = 0x0103
+.equ lap_seconds = 0x0104
+.equ lap_minutes = 0x0105
 
 ; ------------------------------------------------------------
 ; Vector Table
@@ -53,7 +55,7 @@
 ; Strings and lookup-tables
 ;--------------------------------------------------------
 colon: .db ":", 0                       ; allocates bytes for ascii chars with terminator(0) ending the string
-lap: .db "Lap: " , 0
+lap: .db "Split: " , 0
 
 ;--------------------------------------------------------
 ; Includes
@@ -75,15 +77,19 @@ main:
           sts seconds, r16
           sts minutes, r16
 
+          sts lap_centiseconds, r16
+          sts lap_seconds, r16
+          sts lap_minutes, r16
+
           ; init timer, lcd and gpio registers
           rcall     gpio_init           ; buttons
           rcall     LCD_INIT            ; LCD screen
-          ;rcall     RAND_INIT           ; random number generator, NOT NEEDED?
           rcall     timer_init          ; initi
 
           clr       startFlag           ; startFlag = false
           clr       tickFlag            ; tickFlag = false
           clr       lapFlag             ; lapFlag = false
+          clr       state               ; will start in stopped state
 
           sei                           ; enable global interrupts   
                  
@@ -107,21 +113,22 @@ sw_run:
           ldi       state, running
           clr       startFlag
 
-
+          
 check_tick:
           ; occurs every 10 ms, 0.01
           tst       tickFlag            ; wait for next tick
-          breq      end_main            ; back to loop if tick hasn't occurred
+          breq      main_loop            ; back to loop if tick hasn't occurred // Edited from end_main to main_loop for out of range err
 
 run_logic:
           tst       state               
-          breq      stop_logic          ; branch if stopwatch is stopped
+          brne      PC+2                ; using inverted condition and PC+2 for out of reach error, though considered bad practice  
+          rjmp      stop_logic          ; branch if stopwatch is stopped // Changed from breq to rjmp 
 
           ;Logic when stopwatch is running
           
           ; reset LCD screen
-          ;rcall     LCD_CLEAR
-          rcall     LCD_HOME
+          ;rcall     LCD_CLEAR           ; causes flickers
+          rcall     LCD_HOME             ; // REVISE 
 
           ;update centiseconds
           lds       r17, centiseconds    ; load into register from address
@@ -131,7 +138,7 @@ run_logic:
           breq      update_seconds      ; branch to update other time values
 
           sts       centiseconds, r17    ; store values back into centisecond
-          rjmp      output              ; back to loop if 100 centiseconds is not met
+          rjmp      output              ; back to loop if 100 centiseconds is not met  
 
 update_seconds:
           clr       r17
@@ -145,7 +152,7 @@ update_seconds:
           breq      update_minutes      ; branch to update other time values
 
           sts       seconds, r18
-          rjmp      output              ; back to loop if 60 seconds is not met
+          rjmp      output              ; back to loop if 60 seconds is not met  
 
 update_minutes:
           clr       r18
@@ -155,21 +162,36 @@ update_minutes:
           lds       r19, minutes        ; load into register from address
           inc       r19                  ; increases value
 
-;          cpi       r2, 60              ; compare to 60
           sts       minutes, r19         ; branch to update other time values
-
-;          sts       seconds, r1
- ;         rjmp      end_loop            ; back to loop if 60 seconds is not met
-          ; update
-          
+          rcall     LCD_CLEAR
 
 output:
+                   
+          tst       lapFlag             ; testing the lapFlag for branching
+          breq      display   // TEST
+
+          ; Essentially takes a "snapshot" of all the timer details to display at output_split
+          lds       r20, centiseconds
+          sts       lap_centiseconds, r20
+          lds       r20, seconds
+          sts       lap_seconds, r20
+          lds       r20, minutes
+          sts       lap_minutes, r20
+          rcall     output_split
+
+display:
+          
+          ; Set cursor to row 1 col 0 (reference lcd.inc)
+          ldi       LCD_DATA, 0x80
+          rcall     LCD_SEND_COMMAND
+
           ; loads time values
           lds       r19, minutes
           lds       r18, seconds
           lds       r17, centiseconds
 
           ; print minutes
+          clr       r31                         ; // REV  add clr r31 to all three times?
           mov       r30, r19
           rcall     LCD_PRINT_UINT16
 
@@ -179,6 +201,7 @@ output:
           rcall     LCD_WRITE_STRING_PM
 
           ; print seconds
+          clr       r31
           mov       r30, r18
           rcall     LCD_PRINT_UINT16
 
@@ -188,9 +211,9 @@ output:
           rcall     LCD_WRITE_STRING_PM
 
           ; print centiseconds
+          clr       r31
           mov       r30, r17
           rcall     LCD_PRINT_UINT16
-
 
 stop_logic:
           tst       state
@@ -199,8 +222,8 @@ stop_logic:
 
 end_loop:
           
-
           clr       tickFlag            ; tickFlag = false
+          clr       lapFlag             ; lapFlag = false
 
 end_main:
           rjmp      main_loop
@@ -210,18 +233,41 @@ end_main:
 gpio_init:
 ; ------------------------------------------------------------
           ; initialize buttons
+
+          
+          ; Configure INT0 start pause button (PD2)
           cbi       BTN_DIR, BTN_START  ; input mode
           sbi       BTN_MODE, BTN_START ; pull-up
+
+          ; Configure INT1 LAP BUTTON (PD3)
+          cbi       BTN_DIR, BTN_LAP    ; input mode
+          sbi       BTN_MODE, BTN_LAP   ; pull-up
+
+          ldi       r20, (0b10 << ISC00); fall-edge trigger
+          ori       r20, (0b10 << ISC10); fall-edge trigger
+          sts       EICRA, r20          ; set sense bits
+
+          sbi       EIMSK, INT0         ; enable INT0, pd2?
+          sbi       EIMSK, INT1         ; enable INT1, pd3
+
+          /*
+          
+          ; initialize buttons
+          cbi       BTN_DIR, BTN_START  ; input mode
+          sbi       BTN_MODE, BTN_START ; pull-up
+
           sbi       EIMSK, INT0         ; enable INT0, pd2?
           ldi       r20, (0b10 << ISC00); fall-edge trigger
           sts       EICRA, r20          ; set sense bits
 
+          ; Configure INT1 LAP BUTTON (PD3)
           cbi       BTN_DIR, BTN_LAP    ; input mode
           sbi       BTN_MODE, BTN_LAP   ; pull-up
+
           sbi       EIMSK, INT1         ; enable INT1, pd3?
           ori       r20, (0b10 << ISC10); fall-edge trigger
-
           sts       EICRA, r20          ; set sense bits
+          */
 
           ret
 
@@ -305,6 +351,8 @@ lap_ISR:
           in        r20, SREG
           push      r20
 
+          ;rcall     LCD_CLEAR  ; troubleshooting
+
           ldi       lapFlag, 1          ; readFlag = true
 
           ; Restore saved SREG and restore working register
@@ -314,6 +362,48 @@ lap_ISR:
           
           reti
 
+; ------------------------------------------------------------
+output_split:
+; ------------------------------------------------------------
+
+          ; Referenced (lcd.inc LCD_GOTO_LINE2)
+          ldi       LCD_DATA, 0xC0
+          rcall     LCD_SEND_COMMAND
+
+          ; Ensuring "Split: " is written in second row
+          ldi       ZH, HIGH(lap << 1)
+          ldi       ZL, LOW(lap << 1)
+          rcall    LCD_WRITE_STRING_PM
+
+          ;print the lap minutes
+          clr       r31                 ; // Rev
+          lds       r30, lap_minutes
+          rcall     LCD_PRINT_UINT16
+          
+          ;print the colon
+          ldi       ZH, HIGH(colon << 1)
+          ldi       ZL, LOW(colon << 1)
+          rcall     LCD_WRITE_STRING_PM
+          
+          ;printing the lap seconds
+          clr       r31                 ; // Rev
+          lds       r30, lap_seconds
+          rcall     LCD_PRINT_UINT16
+          
+          ; print colon
+          ldi       ZH, HIGH(colon << 1)
+          ldi       ZL, LOW(colon << 1)
+          rcall     LCD_WRITE_STRING_PM
+          
+          ; print lap centiseconds
+          clr       r31                 ; // Rev
+          lds       r30, lap_centiseconds
+          rcall     LCD_PRINT_UINT16
+          
+          ret        
+
+          
+          
 ; ------------------------------------------------------------
 ;inc_time:                               ; change time
 ; ------------------------------------------------------------
